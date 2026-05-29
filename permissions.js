b0fe7ddb7829b71d7b2a1fd97912ea517a073920
javisdinh0 <125231789+javisdinh0@users.noreply.github.com>
2026-05-29 16:22:10 +0700

    Update permissions.js

--- permissions.js
+++ permissions.js
@@ -3,7 +3,7 @@ export const userPermissions = {
   "dinhvietdung.amc@gmail.com": 1,
   
   // Cấp độ 2: Chỉ được trả lời (chỉ sửa các cột Trả lời, Ghi chú, Trạng thái)
-  "anhnguyendinh3308@outlook.com": 2,
+  "anhnguyendinh2308@outlook.com": 2,
   
   // Cấp độ 3: Chỉ xem (không sửa được bất kỳ cột nào). 
   // Tất cả các email không có trong danh sách này sẽ tự động được gán cấp độ 3.
